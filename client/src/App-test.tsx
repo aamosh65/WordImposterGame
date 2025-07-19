e06f5fa48a1865287d4b07e54e48a8bd07a2b9a4
Aamosh <magaramosh65@gmail.com>
2025-07-19 21:25:10 +0545

    Update test app and add package-lock.json

--- client/src/App-test.tsx
+++ client/src/App-test.tsx
@@ -8,9 +8,7 @@ function App() {
     <div className="app-container">
       <h1>Word Imposter Game - Test</h1>
       <p>If you can see this, React is working!</p>
-      <button onClick={() => setCount(count + 1)}>
-        Count: {count}
-      </button>
+      <button onClick={() => setCount(count + 1)}>Count: {count}</button>
       <p>Backend URL would be: {window.location.hostname}</p>
     </div>
   );
